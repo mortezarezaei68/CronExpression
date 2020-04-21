b20af683d026651c24495ba1ecbb82f9f2da93ca
morteza <morteza.rezaei68@gmail.com>
2020-04-22 00:12:10 +0430

    changes all codes

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { MaterialModule } from './material-angular/material/material.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
